65419750267ae6f2ad370026328c97f3ebf36638
princebishtcodes <princebisht122@gmail.com>
2026-02-02 11:33:35 +0530

    Deque

--- 01_STLinCPP/04_deque.cpp
+++ 01_STLinCPP/04_deque.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     // Deque stands for Double-Ended Queue. It's a sequence container that allows you to add or remove elements efficiently from both the front and the back.
-    deque<int> dq;
+    deque<int> dq = {12, 43, 65, 87, 98};
     // INSERTION
     // 1. dq.push_back(5) : Pushes 5 at the end
     dq.push_back(5);
@@ -21,6 +21,13 @@ int main()
     dq.pop_back();
     // dq.pop_front() : Removes Element from front
     dq.pop_front();
+
+    // ACCESSING
+
+    // dq.front() : Returns the 1st Element, return type : int
+    cout << dq.front() << endl;
+    // dq.back() : Returns the last Element, return type : int
+    cout << dq.back() << endl;
 
     for (auto it : dq)
     {
