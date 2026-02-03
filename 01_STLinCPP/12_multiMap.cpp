acc0c5ab22d3f62b9a50218d02225220808071ba
princebishtcodes <princebisht122@gmail.com>
2026-02-03 20:30:06 +0530

    Mulitmap

---
+++ 01_STLinCPP/12_multiMap.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Multimap is same as map, just stores duplicate keys and mM[key] = value, can't be used
+    multimap<int, string> mM;
+    return 0;
+}
