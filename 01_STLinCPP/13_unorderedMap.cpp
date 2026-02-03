1260ee1d0516e17823002faaa22e15fd3f25a3ff
princebishtcodes <princebisht122@gmail.com>
2026-02-03 20:34:58 +0530

    Unordered Map

---
+++ 01_STLinCPP/13_unorderedMap.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Unordered Map is just like unordered set, random, also doesn't store duplicates.
+    unordered_map<int, string> um;
+    // upper_bound() and lower_bound() doesn't work here
+    return 0;
+}
