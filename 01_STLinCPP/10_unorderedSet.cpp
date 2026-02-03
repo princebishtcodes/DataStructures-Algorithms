6c25ec2656b1f7f801685f328b862faffbbe43a8
princebishtcodes <princebisht122@gmail.com>
2026-02-03 20:05:29 +0530

    Unordered Sets

---
+++ 01_STLinCPP/10_unorderedSet.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Unordered Set is a container same as set, just stores elements in unsorted/random way, but avoids duplicates
+    unordered_set<int> us;
+    //  Only upper-bound and lower_bound function doesn't work here, rest just like sets
+    // Has better tc and sc then sets, in constant time, just for some worst cases, it can be linear.
+    return 0;
+}
