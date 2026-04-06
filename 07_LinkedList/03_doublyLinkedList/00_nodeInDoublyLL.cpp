b2d681b986ecfacfe9adaee6646dd9edea7cfd9c
princebishtcodes <princebisht122@gmail.com>
2026-04-06 14:53:53 +0530

    Node Implimentation

---
+++ 07_LinkedList/03_doublyLinkedList/00_nodeInDoublyLL.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+int main()
+{
+    return 0;
+}
