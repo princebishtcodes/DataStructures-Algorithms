404ab90a510a188e7fac52a1f47e0f87d2c3e972
princebishtcodes <princebisht122@gmail.com>
2026-04-06 14:54:01 +0530

    Array to DLL

---
+++ 07_LinkedList/03_doublyLinkedList/01_arrayToDoublyLL.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+Node *arrayToDoublyLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *previous = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        temp->prev = previous;
+        previous->next = temp;
+        previous = previous->next;
+    }
+    return head;
+}
+
+void traversal(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+int main()
+{
+    vector<int> arr = {10, 20, 30, 40, 50};
+    Node *head = arrayToDoublyLL(arr);
+    traversal(head);
+    return 0;
+}
